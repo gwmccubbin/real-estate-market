ff751b8f5cfa2c2ed68fddcc7c33d1a400bc25e7
gwmccubbin <gwmccubbin@gmail.com>
2020-04-23 15:29:06 -0500

    Updates Property in smart contract

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
-    uint public productCount = 0;
-    mapping(uint => Product) public products;
+    uint public propertyCount = 0;
+    mapping(uint => Property) public propertys;
 
-    struct Product {
+    struct Property {
         uint id;
         string name;
         uint price;
@@ -13,7 +13,7 @@ contract Marketplace {
         bool purchased;
     }
 
-    event ProductCreated(
+    event PropertyCreated(
         uint id,
         string name,
         uint price,
@@ -21,7 +21,7 @@ contract Marketplace {
         bool purchased
     );
 
-    event ProductPurchased(
+    event PropertyPurchased(
         uint id,
         string name,
         uint price,
@@ -33,41 +33,41 @@ contract Marketplace {
         name = "Dapp University Marketplace";
     }
 
-    function createProduct(string memory _name, uint _price) public {
+    function createProperty(string memory _name, uint _price) public {
         // Require a valid name
         require(bytes(_name).length > 0);
         // Require a valid price
         require(_price > 0);
-        // Increment product count
-        productCount ++;
-        // Create the product
-        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        // Increment property count
+        propertyCount ++;
+        // Create the property
+        propertys[propertyCount] = Property(propertyCount, _name, _price, msg.sender, false);
         // Trigger an event
-        emit ProductCreated(productCount, _name, _price, msg.sender, false);
+        emit PropertyCreated(propertyCount, _name, _price, msg.sender, false);
     }
 
-    function purchaseProduct(uint _id) public payable {
-        // Fetch the product
-        Product memory _product = products[_id];
+    function purchaseProperty(uint _id) public payable {
+        // Fetch the property
+        Property memory _property = propertys[_id];
         // Fetch the owner
-        address payable _seller = _product.owner;
-        // Make sure the product has a valid id
-        require(_product.id > 0 && _product.id <= productCount);
+        address payable _seller = _property.owner;
+        // Make sure the property has a valid id
+        require(_property.id > 0 && _property.id <= propertyCount);
         // Require that there is enough Ether in the transaction
-        require(msg.value >= _product.price);
-        // Require that the product has not been purchased already
-        require(!_product.purchased);
+        require(msg.value >= _property.price);
+        // Require that the property has not been purchased already
+        require(!_property.purchased);
         // Require that the buyer is not the seller
         require(_seller != msg.sender);
         // Transfer ownership to the buyer
-        _product.owner = msg.sender;
+        _property.owner = msg.sender;
         // Mark as purchased
-        _product.purchased = true;
-        // Update the product
-        products[_id] = _product;
+        _property.purchased = true;
+        // Update the property
+        propertys[_id] = _property;
         // Pay the seller by sending them Ether
         address(_seller).transfer(msg.value);
         // Trigger an event
-        emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
+        emit PropertyPurchased(propertyCount, _property.name, _property.price, msg.sender, true);
     }
 }
